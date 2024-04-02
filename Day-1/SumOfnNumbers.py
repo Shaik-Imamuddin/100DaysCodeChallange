9631d51f5f7a771c69474a4c91461e26a1ccec24
Imamuddin Shaik <120667356+Shaik-Imamuudin@users.noreply.github.com>
2024-04-02 19:39:39 +0530

    Add files via upload

---
+++ Day-1/SumOfnNumbers.py
@@ -0,0 +1,28 @@
+#sum of n numbers
+n=int(input("Enter n value:"))
+sum=0
+for i in range(n+1):
+    sum+=i          #time complexity: O(n)
+print(sum)
+
+
+n=int(input("Enter n value:"))
+sum=0
+for i in range(n):
+    for i in range(i):
+        sum+=i          #time complexity: O(n^2)
+print(sum)
+
+
+
+n=int(input("Enter n value:"))
+sum= n*(n+1)//2     #time complexity: O(1)
+print(sum)
+
+n=int(input("Enter n value:"))
+print(sum(range(n+1)))
+
+
+#BestCase = O(1) - Constant Time
+#BestCase = O(n) - Linear Time
+#Average Case = O(n^2) - Quadratic Time
