b785f1771d4af91305876a78c64998f1a1e6ea98
Immu <shaikimam37302@gmail.com>
2024-06-26 12:11:23 +0530

    Adding Day-083 Src Code

---
+++ Day-083/MessiGoals.py
@@ -0,0 +1,13 @@
+#By using Addition operation
+laLiga=int(input())
+copaDelRey=int(input())         #Time Complexity: O(1)
+championsLeague=int(input())
+print(laLiga+copaDelRey+championsLeague)
+
+print("------------------------------")
+
+#By using sum of list elements
+laLiga=int(input())
+copaDelRey=int(input())     #Time Complexity: O(1)
+championsLeague=int(input())
+print(sum([laLiga, copaDelRey, championsLeague]))
